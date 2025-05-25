52e99ef003b8b069677126d77f7f035f77d90771
oralmpa <orestis.almpa@gmail.com>
2025-05-25 22:26:46 +0300

    Fix: improve extractFileName to prioritize 'filename' over 'name'

--- frontend/react-app/src/utils/fileHelpers.js
+++ frontend/react-app/src/utils/fileHelpers.js
@@ -2,7 +2,11 @@
 export const extractFileName = (fileObj) => {
     if (!fileObj) return "Unknown File";
     if (fileObj.name) return fileObj.name;
+    if (fileObj.filename) return fileObj.filename; 
     if (fileObj.file?.name) return fileObj.file.name;
+    if (fileObj.file?.filename) return fileObj.file.filename;
+
+    
     return "Unknown File";
   };
   
