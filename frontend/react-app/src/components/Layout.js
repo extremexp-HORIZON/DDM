980f4c5f8ace72782900a5a12603a6bdae2500d5
oralmpa <orestis.almpa@gmail.com>
2025-06-18 03:53:32 +0300

    update layout to support collapsible sidebar

--- frontend/react-app/src/components/Layout.js
+++ frontend/react-app/src/components/Layout.js
@@ -1,11 +1,19 @@
-import React from "react";
-import Sidebar from "./Sidebar";
+import React, { useState } from "react";
+import CustomSidebar from "./Sidebar";
 
 const Layout = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <div className="layout-container">
-      <Sidebar />
-      <div className="content-container">
+    <div className="layout-container flex">
+      <CustomSidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
+      <div
+        className="content-container transition-all duration-300"
+        style={{
+          marginLeft: isSidebarOpen ? "16rem" : "3.5rem", // match sidebar width
+          width: `calc(100% - ${isSidebarOpen ? "16rem" : "3.5rem"})`,
+        }}
+      >
         {children}
       </div>
     </div>
